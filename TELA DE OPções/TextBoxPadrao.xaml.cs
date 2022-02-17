0b55f7787988840466ea8a52c871285879830769
davyMendes <82676985+davyMendes@users.noreply.github.com>
2022-02-17 16:45:33 -0300

    teste

--- TELA DE OPções/TextBoxPadrao.xaml.cs
+++ TELA DE OPções/TextBoxPadrao.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -29,7 +30,11 @@ namespace TELA_DE_OPções
         public SolidColorBrush CorSecundaria { get; set; }
 
         private string texto { get; set; }
-        public bool estahLivre { get => String.IsNullOrWhiteSpace(this.texto); }
+        //public bool estahLivre { get => String.IsNullOrWhiteSpace(this.texto);}
+        public bool estahLivre { get => true;}
+
+        private Storyboard StoryBoardAtivando;
+        private Storyboard StoryBoardDesativando;
 
         public TextBoxPadrao()
         {
@@ -43,10 +48,44 @@ namespace TELA_DE_OPções
 
         private void tbText_LostFocus(object sender, RoutedEventArgs e)
         {
-            if (string.IsNullOrEmpty(texto))
-            {
-               
-            }
+            AnimacaoQuandoDaFocoNaTextBox();
+
+            //StoryBoardAtivando.Begin();
+        }
+
+        private void AnimacaoQuandoDaFocoNaTextBox()
+        {
+            //var animacao = new ColorAnimation()
+            //{
+            //    Duration = new Duration(TimeSpan.FromSeconds(5)),
+            //    To = System.Windows.Media.Color.FromRgb(255, 255, 255), 
+            //};
+            //StoryBoardAtivando = new Storyboard();
+            //StoryBoardAtivando.Children.Add(animacao);
+            //Storyboard.SetTargetName(animacao, bdText.Name);
+            //var prop = new PropertyPath(Border.BackgroundProperty);
+            //Storyboard.SetTargetProperty(animacao, prop);
+
+            //< ThicknessAnimation
+            //                      Storyboard.TargetName = "lbTitulo"
+            //                      Storyboard.TargetProperty = "(Label.Margin)"
+            //                      To = "0 -13 0 0"
+            //                      Duration = "0:0:0.3"         
+            //var animacao = new ThicknessAnimation()
+            //{
+            //    Duration = new Duration(TimeSpan.FromSeconds(5)),
+            //    To =new Thickness(0, -5, 0, 0),
+            //};
+            //StoryBoardAtivando = new Storyboard();
+            //StoryBoardAtivando.Children.Add(animacao);
+            //Storyboard.SetTargetName(animacao, this.Template.Templa.Name);
+            //var prop = new PropertyPath(TextBlock.MarginProperty);
+            //Storyboard.SetTargetProperty(animacao, prop);
+        }
+
+        private void AnimacaoQuandoTiraOFocoNaTextBox()
+        {
+
         }
     }
 }
