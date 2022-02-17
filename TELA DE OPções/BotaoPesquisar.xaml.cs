524f39634b5b0c6a948a007848f19b8f40553412
davyMendes <82676985+davyMendes@users.noreply.github.com>
2022-02-17 09:48:43 -0300

    jesus

---
+++ TELA DE OPções/BotaoPesquisar.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TELA_DE_OPções
+{
+    /// <summary>
+    /// Interação lógica para BotaoPesquisar.xam
+    /// </summary>
+    public partial class BotaoPesquisar : UserControl
+    {
+        public static readonly DependencyProperty NormalImageProperty = DependencyProperty.Register("NormalImage", typeof(ImageSource), typeof(BotaoPesquisar), new UIPropertyMetadata(null));
+
+        public static readonly DependencyProperty TextoPrincipalProperty = DependencyProperty.Register("TextoPrincipal", typeof(string), typeof(BotaoPesquisar), new UIPropertyMetadata(null));
+
+        public static readonly DependencyProperty TextoSecundarioProperty = DependencyProperty.Register("TextoSecundario", typeof(string), typeof(BotaoPesquisar), new UIPropertyMetadata(null));
+
+        public static readonly DependencyProperty CorPrimariaProperty = DependencyProperty.Register("CorPrimaria", typeof(SolidColorBrush), typeof(BotaoPesquisar), new UIPropertyMetadata(null));
+
+        public static readonly DependencyProperty CorSecundariaProperty = DependencyProperty.Register("CorSecundaria", typeof(SolidColorBrush), typeof(BotaoPesquisar), new UIPropertyMetadata(null));
+
+        public ImageSource NormalImage
+        {
+            get { return (ImageSource)GetValue(NormalImageProperty); }
+            set { SetValue(NormalImageProperty, value); }
+        }
+        public string TextoPrincipal { get; set; }
+        public string TextoSecundario { get; set; }
+        public SolidColorBrush CorPrincipal { get; set; }
+        public SolidColorBrush CorSecundaria { get; set; }
+
+        public BotaoPesquisar()
+        {        
+            InitializeComponent();
+        }
+
+
+        public event RoutedEventHandler Click;
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Click != null)
+            {
+                Click(this, e);
+            }
+        }
+    }
+}
---
+++ TELA DE OPções/TextBoxPadrao.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TELA_DE_OPções
+{
+    public partial class TextBoxPadrao : UserControl
+    {
+
+        public static readonly DependencyProperty TextoPrincipalProperty = DependencyProperty.Register("Titulo", typeof(string), typeof(TextBoxPadrao), new UIPropertyMetadata(null));
+
+        public static readonly DependencyProperty CorPrimariaProperty = DependencyProperty.Register("CorPrincipal", typeof(SolidColorBrush), typeof(TextBoxPadrao), new UIPropertyMetadata(null));
+
+        public static readonly DependencyProperty CorSecundariaProperty = DependencyProperty.Register("CorSecundaria", typeof(SolidColorBrush), typeof(TextBoxPadrao), new UIPropertyMetadata(null));
+
+        public string Titulo { get; set; }
+        public SolidColorBrush CorPrincipal { get; set; }
+        public SolidColorBrush CorSecundaria { get; set; }
+
+        private string texto { get; set; }
+        public bool estahLivre { get => String.IsNullOrWhiteSpace(this.texto); }
+
+        public TextBoxPadrao()
+        {
+            texto = "";
+            InitializeComponent();
+        }
+        private void tbText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            texto = ((TextBox)sender).Text.Trim();
+        }
+
+        private void tbText_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(texto))
+            {
+               
+            }
+        }
+    }
+}
