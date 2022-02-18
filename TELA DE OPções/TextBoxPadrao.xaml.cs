7758d6cfed543c12381801b218ef030b8eb5fc69
davyMendes <82676985+davyMendes@users.noreply.github.com>
2022-02-18 07:00:47 -0300

    até que fim

--- TELA DE OPções/TextBoxPadrao.xaml.cs
+++ TELA DE OPções/TextBoxPadrao.xaml.cs
@@ -28,64 +28,142 @@ namespace TELA_DE_OPções
         public string Titulo { get; set; }
         public SolidColorBrush CorPrincipal { get; set; }
         public SolidColorBrush CorSecundaria { get; set; }
+        public bool estahLivre { get => String.IsNullOrWhiteSpace(tbText.Text); }
 
-        private string texto { get; set; }
-        //public bool estahLivre { get => String.IsNullOrWhiteSpace(this.texto);}
-        public bool estahLivre { get => true;}
-
-        private Storyboard StoryBoardAtivando;
-        private Storyboard StoryBoardDesativando;
+        private Storyboard AnimacaoAtiva;
+        private Storyboard AnimacaoDesativa;
 
         public TextBoxPadrao()
-        {
-            texto = "";
+        {          
             InitializeComponent();
-        }
-        private void tbText_TextChanged(object sender, TextChangedEventArgs e)
+            AdicionarAnimacaoAtiva();
+            AdicionarAnimacaoDesativa();
+        }      
+        private void tbText_GotFocus(object sender, RoutedEventArgs e)
         {
-            texto = ((TextBox)sender).Text.Trim();
+            AnimacaoAtiva.Begin(lbTitulo);
         }
-
         private void tbText_LostFocus(object sender, RoutedEventArgs e)
         {
-            AnimacaoQuandoDaFocoNaTextBox();
-
-            //StoryBoardAtivando.Begin();
+            if (estahLivre)
+                AnimacaoDesativa.Begin(lbTitulo);
         }
 
-        private void AnimacaoQuandoDaFocoNaTextBox()
+        private void AdicionarAnimacaoAtiva()
         {
-            //var animacao = new ColorAnimation()
-            //{
-            //    Duration = new Duration(TimeSpan.FromSeconds(5)),
-            //    To = System.Windows.Media.Color.FromRgb(255, 255, 255), 
-            //};
-            //StoryBoardAtivando = new Storyboard();
-            //StoryBoardAtivando.Children.Add(animacao);
-            //Storyboard.SetTargetName(animacao, bdText.Name);
-            //var prop = new PropertyPath(Border.BackgroundProperty);
-            //Storyboard.SetTargetProperty(animacao, prop);
+            var duracao = new Duration(TimeSpan.FromMilliseconds(300));
 
-            //< ThicknessAnimation
-            //                      Storyboard.TargetName = "lbTitulo"
-            //                      Storyboard.TargetProperty = "(Label.Margin)"
-            //                      To = "0 -13 0 0"
-            //                      Duration = "0:0:0.3"         
-            //var animacao = new ThicknessAnimation()
-            //{
-            //    Duration = new Duration(TimeSpan.FromSeconds(5)),
-            //    To =new Thickness(0, -5, 0, 0),
-            //};
-            //StoryBoardAtivando = new Storyboard();
-            //StoryBoardAtivando.Children.Add(animacao);
-            //Storyboard.SetTargetName(animacao, this.Template.Templa.Name);
-            //var prop = new PropertyPath(TextBlock.MarginProperty);
-            //Storyboard.SetTargetProperty(animacao, prop);
+            ThicknessAnimation animacaoReposiciona = new ThicknessAnimation()
+            {
+                Duration = duracao,
+                To = new Thickness(5, -8, 0, 0)
+            };
+
+            Storyboard.SetTargetName(animacaoReposiciona, "lbTitulo");
+            Storyboard.SetTargetProperty(animacaoReposiciona, new PropertyPath(TextBlock.MarginProperty));
+
+
+            DoubleAnimation animationAumentaTamanho1 = new DoubleAnimation()
+            {
+                Duration = duracao,
+                To = 10
+            };
+            Storyboard.SetTargetName(animationAumentaTamanho1, "lbTitulo");
+            Storyboard.SetTargetProperty(animationAumentaTamanho1, new PropertyPath(TextBlock.FontSizeProperty));
+
+
+            DoubleAnimation animationAumentaTamanho2 = new DoubleAnimation()
+            {
+                Duration = duracao,
+                To = 1
+            };
+            Storyboard.SetTargetName(animationAumentaTamanho2, "lbTitulo");
+            Storyboard.SetTargetProperty(animationAumentaTamanho2, new PropertyPath(TextBlock.OpacityProperty));
+
+            ColorAnimation animacaoCor = new ColorAnimation()
+            {
+                Duration = duracao,
+                To = Color.FromRgb(2, 117, 216)
+            };
+
+            Storyboard.SetTargetName(animacaoCor, "lbTitulo");
+            Storyboard.SetTargetProperty(animacaoCor, new PropertyPath("(0).(1)", TextBlock.ForegroundProperty, SolidColorBrush.ColorProperty));
+
+            ColorAnimation animacaoCor2 = new ColorAnimation()
+            {
+                Duration = duracao,
+                To = Color.FromRgb(2, 117, 216)
+            };
+
+            Storyboard.SetTargetName(animacaoCor2, "bdText");
+            Storyboard.SetTargetProperty(animacaoCor2, new PropertyPath("(0).(1)", Border.BorderBrushProperty, SolidColorBrush.ColorProperty));
+
+
+            AnimacaoAtiva = new Storyboard();
+
+            AnimacaoAtiva.Children.Clear();
+            AnimacaoAtiva.Children.Add(animacaoReposiciona);
+            AnimacaoAtiva.Children.Add(animationAumentaTamanho1);
+            AnimacaoAtiva.Children.Add(animationAumentaTamanho2);
+            AnimacaoAtiva.Children.Add(animacaoCor);
+            AnimacaoAtiva.Children.Add(animacaoCor2);
         }
-
-        private void AnimacaoQuandoTiraOFocoNaTextBox()
+        private void AdicionarAnimacaoDesativa()
         {
+            var duracao = new Duration(TimeSpan.FromMilliseconds(300));
 
-        }
+            ThicknessAnimation animacaoReposiciona = new ThicknessAnimation()
+            {
+                Duration = duracao,
+                To = new Thickness(5, 0, 0, 0)
+            };
+
+            Storyboard.SetTargetName(animacaoReposiciona, "lbTitulo");
+            Storyboard.SetTargetProperty(animacaoReposiciona, new PropertyPath(TextBlock.MarginProperty));
+
+            DoubleAnimation animationAumentaTamanho1 = new DoubleAnimation()
+            {
+                Duration = duracao,
+                To = 18
+            };
+            Storyboard.SetTargetName(animationAumentaTamanho1, "lbTitulo");
+            Storyboard.SetTargetProperty(animationAumentaTamanho1, new PropertyPath(TextBlock.FontSizeProperty));
+
+
+            DoubleAnimation animationAumentaTamanho2 = new DoubleAnimation()
+            {
+                Duration = duracao,
+                To = 0.5
+            };
+            Storyboard.SetTargetName(animationAumentaTamanho2, "lbTitulo");
+            Storyboard.SetTargetProperty(animationAumentaTamanho2, new PropertyPath(TextBlock.OpacityProperty));
+
+            ColorAnimation animacaoCor = new ColorAnimation()
+            {
+                Duration = duracao,
+                To = Colors.DimGray
+            };
+
+            Storyboard.SetTargetName(animacaoCor, "lbTitulo");
+            Storyboard.SetTargetProperty(animacaoCor, new PropertyPath("(0).(1)", TextBlock.ForegroundProperty, SolidColorBrush.ColorProperty));
+
+            ColorAnimation animacaoCor2 = new ColorAnimation()
+            {
+                Duration = duracao,
+                To = Colors.DimGray
+            };
+
+            Storyboard.SetTargetName(animacaoCor2, "bdText");
+            Storyboard.SetTargetProperty(animacaoCor2, new PropertyPath("(0).(1)", Border.BorderBrushProperty, SolidColorBrush.ColorProperty));
+
+            AnimacaoDesativa = new Storyboard();
+
+            AnimacaoDesativa.Children.Clear();
+            AnimacaoDesativa.Children.Add(animacaoReposiciona);
+            AnimacaoDesativa.Children.Add(animationAumentaTamanho1);
+            AnimacaoDesativa.Children.Add(animationAumentaTamanho2);
+            AnimacaoDesativa.Children.Add(animacaoCor);
+            AnimacaoDesativa.Children.Add(animacaoCor2);
+        }    
     }
 }
